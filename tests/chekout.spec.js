c52b8d63c607845756b56393044c8aee594d5b78
VVladLytvyn <1997vlad1997bob@gmail.com>
2025-10-26 13:30:50 +0200

    some changes

--- tests/chekout.spec.js
+++ tests/chekout.spec.js
@@ -8,22 +8,21 @@ import { faker } from "@faker-js/faker";
 
 describe("Valid Checkout", () => {
   it("should complete checkout successfully", async () => {
-    // Precondition: login
     await browser.url("https://www.saucedemo.com/");
     await LoginPage.login("standard_user", "secret_sauce");
 
-    // Step 1: add product to cart
+    // add product to cart
     await InventoryPage.addToCart("sauce-labs-backpack");
 
-    // Step 2: go to cart
+    // go to cart
     await InventoryPage.goToCart();
     const cartItems = await CartPage.getCartItemNames();
     expect(cartItems).toContain("Sauce Labs Backpack");
 
-    // Step 3: click Checkout
+    // click Checkout
     await CartPage.clickCheckout();
 
-    // Step 4-6: fill checkout form
+    // fill checkout form
     await CheckoutStep1Page.fillForm(
       faker.person.firstName(),
       faker.person.lastName(),
@@ -31,14 +30,14 @@ describe("Valid Checkout", () => {
     );
     await CheckoutStep1Page.clickContinue();
 
-    // Step 7: Finish
+    // Finish
     await CheckoutStep2Page.clickFinish();
 
-    // Step 8: Verify order complete
+    // Verify order complete
     const successMessage = await CheckoutCompletePage.getSuccessMessage();
     expect(successMessage).toBe("Thank you for your order!");
 
-    // Step 9: Back Home
+    // Back Home
     await CheckoutCompletePage.clickBackHome();
     await InventoryPage.verifyOnInventoryPage();
   });
